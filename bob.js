bf432e4ae755656417037fe53d84ff77f73fe60c
ultamarenet <71134107+ultamarenet@users.noreply.github.com>
2020-10-25 19:09:55 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,21 @@
+class Bob {
+    constructor(x,y) {
+      var options = {
+          isStatic:false,
+          restitution:0.3,
+          friction:0.5,
+          density:1.2
+      }
+      this.body = Bodies.circle(x,y,20,options);
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x,pos.y);
+      ellipseMode(CENTER);
+      fill("yellow");
+      ellipse(0, 0, this.width );
+      pop();
+    }
+  };
